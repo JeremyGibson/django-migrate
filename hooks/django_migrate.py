6b237df0ececdecbbb7826c64d4219ae6fa2a92b
Jeremy Gibson <jgibson@caktusgroup.com>
2020-05-14 08:19:40 -0400

    Push commit

---
+++ hooks/django_migrate.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+from pathlib import Path
+
+def main():
+    exitcode = 0
+    cp = subprocess.run(
+        ["python", "manage.py", "makemigrations", "--dry-run", "/dev/null"], 
+        capture_output=True
+    )
+    if cp.stdout:
+        if not cp.stdout.__contains__("No changes detected"):
+            exitcode = 1
+    return exitcode
+
+if __name__ == "__main__":
+    exit(main())
---
+++ setup.py
@@ -0,0 +1,25 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+setup()
+# from setuptools import setup, find_packages
+
+
+# setup(
+#     name='django-migrations',
+#     version='0.0.1',
+#     packages=find_packages(),
+#     entry_points = {
+#         'console_scripts': ['django_migrate=django_migrate:check_migrations'],
+#     },
+#     url='https://github.com/caktus/django-migrations',
+#     author='Caktus Group',
+#     author_email='',
+#     description='A pre-commit hook to check a django app for migrations',
+#     python_requires='>=3.5',
+#     classifiers=[
+#         'Intended Audience :: Developers',
+#         'Topic :: Software Development :: Build Tools',
+#         'License :: OSI Approved :: BSD License',
+#         'Programming Language :: Python :: 3',
+#     ]
+# )
